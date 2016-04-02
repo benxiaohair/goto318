e978ced4da8aa2266b02778881bd1702cccfaa1e
stupid boy <494583092@qq.com>
2016-04-03 00:44:29 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,10 +8,10 @@ return array(
 
     //数据库的链接
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => 'qdm175613444.my3w.com', // 服务器地址
-    'DB_NAME'   => 'qdm175613444_db', // 数据库名
-    'DB_USER'   => 'qdm175613444', // 用户名
-    'DB_PWD'    => 'chendongliang', // 密码
+    'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_NAME'   => 'test', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => 'root', // 密码
     'DB_PORT'   => 3306, // 端口
     //'DB_PREFIX' => 'think_', // 数据库表前缀
     //其他项目配置参数
